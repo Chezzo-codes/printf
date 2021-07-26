9dadc50ff4a6e7721b38a33fa19ac15af3d950db
Chezlin Drude <chezlindrude23@gmail.com>
2021-07-26 16:35:19 +0200

    Updated printf.c

--- printf.c
+++ printf.c
@@ -1,50 +1,61 @@
-#include <stdarg.h>
-#include <stdio.h>
-
-typedef struct spec
-{
-    char s;
-    void (func_p*)(int c)
-
-}sp_t;
-
-    sp_t specifier[] = {
-        {"c", print_char},
-        {"s", print_str},
-        {"d", print_int},
-        {"i", print_uint},
-        {"%", get_modifier},
-        {NULL, NULL}
-    };
-    int i;
-/**
- * _printf -
- *
- */
-void _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
     va_list args;
-    va_start(args, format);
+    int len = 0;
+    const char *string;
 
-    while (*format != '\0') {
-
-        if (*format == 'd') {
-            int i = va_arg(args,int);
-            putchar(51);
-        } else if (*format == 'c') {
-            int c = va_arg(args, int);
-            printf( "%c\n", (char)c ) ;
-        } else if (*format == 'f') {
-            double d = va_arg(args, double);
-            printf( "%f\n", d ) ;
-        }
-        format++;
+    if (format == NULL)
+    {
+        return (-1);
     }
 
-    va_end(args);
-}
+    if (format)
+    {
+        va_start(args, format);
+        string = format;
+        while (*format != '\0' && *format != '%')
+        {
+            len = get_strlen(string);
 
-int main()
-{
-    _printf("dcfc", 3, 'a', 1.999, 'v');
+            putchar(*string);
+
+            while (*string != '\0')
+            {
+                putchar(*string);
+                string++;
+            }
+            putchar('\n');
+
+            return (len);
+        }
+
+        if (*format == '%')
+        {
+            string = "Modifier Found";
+            while(*string != '\0')
+            {
+                putchar(*string);
+                string++;
+            }
+        }
+
+            /*if (*format == 'd')
+            {
+                int i = va_arg(args,int);
+                putchar(51);
+            }
+            else if (*format == 'c')
+            {
+                int c = va_arg(args, int);
+                printf( "%c\n", (char)c ) ;
+            }
+            else if (*format == 'f')
+                double d = va_arg(args, double);
+                printf( "%f\n", d ) ;
+            }*/
+            format++;
+        }
+
+    va_end(args);
+    return (get_strlen(string));
 }
