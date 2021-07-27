8a2e4e8dff23cfe8f1d9a957526205e30633bfbd
Chezlin Drude <chezlindrude23@gmail.com>
2021-07-27 09:35:55 +0200

    Fixed

--- _printf.c
+++ _printf.c
@@ -9,67 +9,62 @@
  */
 int _printf(const char*format,... )
 {
-    int len = 0;
-    const char *string;
-    va_list arg;
-    va_start(arg, format);
+	int len = 0;
+	const char *string;
+	va_list arg;
+	va_start(arg, format);
 
-    if (format)
-    {
-        string = format;
-        len = get_strlen(string);
-        while (*format)
-        {
-            if (*format == '%')
-            {
-                format++;
-                switch (*format)
-                {
-            //string
-                    case 's':
-                    {
-                        char *ret = va_arg(arg, char*);
-                        while (*ret)
-                        {
-				_putchar(*ret);
-				ret++;
-                        }
-                    }
-                    break;
-            //character
-                    case 'c':
-                    {
-                        _putchar((char)va_arg(arg, int));
-                    }
-                    break;
-            //number
-                    case 'd':
-                    {
-                        int ret = va_arg(arg, int);
-                        print_int(ret);
-                    }
-                    break;
-            //int
-                    case 'i':
-                    {
-                        int ret = va_arg(arg, int);
-                        print_int(ret);
-                    }
-                    break;
-            //default
-                    default:
-                    {
-                        _putchar(*format);
-                    }
-                    break;
-                }
-            }
-            else
-            {
-                _putchar(*format);
-            }
-            format++;
-        }
-    }
-    return (len);
+	if (format)
+	{
+		string = format;
+		len = get_strlen(string);
+		while (*format)
+		{
+			if (*format == '%')
+			{
+				format++;
+				switch (*format)
+				{
+				case 's':
+				{
+					char *ret = va_arg(arg, char*);
+					while (*ret)
+					{
+						_putchar(*ret);
+						ret++;
+					}
+				}
+				break;
+				case 'c':
+				{
+					_putchar((char)va_arg(arg, int));
+				}
+				break;
+				case 'd':
+				{
+					int ret = va_arg(arg, int);
+					print_int(ret);
+				}
+				break;
+				case 'i':
+				{
+					int ret = va_arg(arg, int);
+					print_int(ret);
+				}
+				break;
+				default:
+				{
+					_putchar(*format);
+				}
+				break;
+				}
+			}
+			else
+			{
+				_putchar(*format);
+			}
+			format++;
+		}
+	}
+	return (len);
 }
--- holberton.h
+++ holberton.h
@@ -1,9 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdarg.h>
 
-int _putchar(char c)
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 int get_strlen(const char *str);
-void print_str(va_list arg);
+void print_int(int num);
 
 #endif
